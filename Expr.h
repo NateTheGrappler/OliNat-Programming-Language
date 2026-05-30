0a0f4b6c3294ce6df5bac79c461c2461c07c33ee
Natan Guzinski <natang@NatansCodingLaptop.lan>
2026-05-30 18:57:00 -0400

    initial commit for vm and expr

---
+++ Expr.h
@@ -0,0 +1,8 @@
+//
+// Created by natang on 5/30/26.
+//
+
+#ifndef OLI_NAT_EXPR_H
+#define OLI_NAT_EXPR_H
+
+#endif //OLI_NAT_EXPR_H
---
+++ vm.h
@@ -0,0 +1,8 @@
+//
+// Created by natang on 5/30/26.
+//
+
+#ifndef OLI_NAT_VM_H
+#define OLI_NAT_VM_H
+
+#endif //OLI_NAT_VM_H
